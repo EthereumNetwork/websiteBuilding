b24b94adbcafe6db4bc5b9b2238b512b1fdc78bb
Michael Ilg <michael@ilg.ac>
2016-12-24 23:47:21 -0800

    Integrated VueJS

---
+++ public/app.js
@@ -0,0 +1,60 @@
+
+const Learn = { template: '<div>Developer Resources coming soon...</div>' };
+const Explorer = { template: '<div>Explorer coming later...</div>' };
+const Dapps = { template: '<div>dapps</div>' };
+
+Vue.component('dapp-item', {
+  props: ['dapp'],
+  template: '<li><h3>{{ dapp.title }}</h3><p>{{dapp.description}}</p></li>'
+});
+
+var app7 = new Vue({
+  el: '#dappListDiv',
+  data: {
+    searchString: "",
+    dappList: [
+      { title: 'Golem', description: 'Golem is great' },
+      { title: 'Augur', description: 'Augur is a prediction market'  },
+      { title: 'Whatever else Ethereum can do', description: 'Ethereum is the new kid on the block'  }
+    ]
+  },
+  computed: {
+    // A computed property that holds only those articles that match the searchString.
+    filteredDapps: function () {
+        var dappList_array = this.dappList,
+            searchString = this.searchString;
+
+        if(!searchString){
+            return dappList_array;
+        }
+
+        searchString = searchString.trim().toLowerCase();
+
+        dappList_array = dappList_array.filter(function(item){
+            if(item.description.toLowerCase().indexOf(searchString) !== -1){
+                return item;
+            }
+        })
+
+        // Return an array with the filtered data.
+        return dappList_array;;
+    }
+  }
+});
+
+
+const routes = [
+  { path: '/learn', component: Learn },
+  { path: '/explorer', component: Explorer },
+  { path: '/dapps', component: Dapps }
+]
+
+const router = new VueRouter({
+  routes: routes,
+   mode: 'history'
+})
+
+
+const app = new Vue({
+  router
+}).$mount('#app')
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var history = require('connect-history-api-fallback');
+
 var app = express();
 
+app.use(history())
 app.use(express.static('public'));
 
 app.listen(3000, function () {
